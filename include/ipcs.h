7fb739ad9f28a20bb936fd773cf1e720aee02a3d
Dercury <dercury@qq.com>
2018-12-25 20:43:38 +0800

    Update ipcs.h

--- include/ipcs.h
+++ include/ipcs.h
@@ -1,3 +1,20 @@
 
-int IPCS_CreateServer(const char* serverName, int (*ResponseProc)(int fd, int cmdId, void* recvData, int recvDataLen));
+int IPCS_CreateServer(const char *serverName, int (*ServerResponseProc)(int fd, unsigned int cmdId, void *recvData, int recvDataLen));
 
+int IPCS_DestroyServer(const char *serverName);
+
+unsigned int IPCS_RespondMessage(int fd, unsigned int cmdId, void *dataAddr, unsigned int dataLength);
+
+
+
+unsigned int IPCS_CreateSyncClient(const char *clientName, const char *serverName, int *fd);
+
+unsigned int IPCS_CreateAsynClient(const char *clientName, const char *serverName, unsigned int (*ClientReponseProc)(unsigned int cmdId, void *recvData, int recvDataLen), int *fd);
+
+int IPCS_DestroyClient(int fd);
+
+
+
+int IPCS_SyncCall(int fd, unsigned int cmdId, void *sendData, unsigned int sendDataLen, unsigned int *recvCmdId, void **recvData, unsigned int *recvDataLen);
+
+int IPCS_AsynCall(int fd, unsigned int cmdId, void *sendData, unsigned int sendDataLen);
