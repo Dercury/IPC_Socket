8c7189f072b995aecd27918e034a15227392a7e0
Dercury <dercury@qq.com>
2018-12-29 17:25:51 +0800

    Update ipcs_common.h

--- src/ipcs_common.h
+++ src/ipcs_common.h
@@ -19,8 +19,29 @@
 #ifndef __IPCS_COMMON_H__
 #define __IPCS_COMMON_H__
 
+#include "ipcs.h"
+
 #define IPCS_SERVER_NAME_MAX_LEN     256
 #define IPCS_CLIENT_NAME_MAX_LEN     256
 
-#endif /* __IPCS_COMMON_H__ */
+typedef struct {
+    unsigned int msgType;
+    unsigned int msgLen;
+} IPCS_MessageHeader;
 
+typedef struct {
+    IPCS_MessageHeader msgHeader;
+    void *msgValue;
+} IPCS_InnerMessage;
+
+void IPCS_WriteLogImpl(const char *filename, unsigned int lineNum, const char *format, ...);
+
+#define IPCS_WriteLog(format, ...)      IPCS_WriteLogImpl(__FILE__, __LINE__, (format), ##__VA_ARGS__)
+
+int IPCS_CreateThread(void *(threadRunFunc)(void *), void *threadArg, pthread_t *threadId);
+
+int IPCS_MsgToStream(IPCS_Message *msg, void *streamBuf, unsigned int *bufLen);
+
+int IPCS_StreamToMsg(void *streamBuf, unsigned int bufLen, IPCS_Message *msg);
+
+#endif /* __IPCS_COMMON_H__ */
